83b0a3caccb4462de264974ba93683b9cb945cd7
Ravi Bhushan <iqravi.bhushan@gmail.com>
2018-08-22 23:46:50 +0530

    First Upload

---
+++ app/src/main/java/com/example/ravi/a8ball/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.ravi.a8ball;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btn_ask = findViewById(R.id.buttonAsk);
+        final TextView textView = findViewById(R.id.textView);
+
+        final ImageView ballDisplay = findViewById(R.id.image_eightBall);
+
+        final int[] ballArray = {R.drawable.ball1
+                ,R.drawable.ball2
+                ,R.drawable.ball3
+                ,R.drawable.ball4,
+                R.drawable.ball5};
+
+        final Random random = new Random(4);
+        btn_ask.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                int number;
+                number  = random.nextInt(4);
+                ballDisplay.setImageResource(ballArray[number]);
+            }
+        });
+
+
+
+
+    }
+}
